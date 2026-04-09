a1e7d0e1cc1f7c44b19cd3f46c82bccfc8e36eb4
Ben Mino <e13123@ism.edu.ec>
2026-04-09 13:48:49 -0500

    Actividad en Clase 1

---
+++ Actividad 1.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+# Este es un comentario de una sola línea en Español Latinoamérica
+print ("Introduzca un valor")
+input_value = input()
+"""Este es un comentario de varias líneas en Español Latinoamérica
+que abarca múltiples espacios."""
+
