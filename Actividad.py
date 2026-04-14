30842a09c8305263fe3bc8bfeff9222190857739
ben_mino <e13123@ism.edu.ec>
2026-04-13 20:39:11 -0500

    Add files via upload

---
+++ Actividad.py
@@ -0,0 +1,32 @@
+# Comprobar la versión de Python
+import sys
+print("Python Version: ")
+print(sys.version)
+
+# Operaciones con los valores de 3 y 4
+a = 3
+b = 4
+
+print("Adición: 3 + 4 =", a+b)
+print("Sustracción: 3 - 4 =", a-b)
+print("Multiplicación: 3 x 4 =", a*b)
+print("Módulo: 3 % 4 =", a%b)
+print("División: 3 / 4 =", a/b)
+print("Potencia: 3^4 =", a**b)
+print("División Entera: 3 // 4 =", a//b)
+
+# Cadenas
+nombre = 'Ben'
+apellido = 'Mino'
+país = 'Ecuador'
+frase = 'Estoy disfrutando de 30 días de Python.'
+
+# Comprobación de tipos de datos
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type("Asabeneh"))
+print(type("Python"))
+print(type("Finland"))
+print(type("Mino"))
+print(type("Ecuador"))
