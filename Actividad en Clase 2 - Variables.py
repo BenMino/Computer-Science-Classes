3e0a522edbcf5f935fffb4f6f2cbddd14f5fa860
Ben Mino <e13123@ism.edu.ec>
2026-04-14 12:24:36 -0500

    Actividad en Clase 2

--- Actividad en Clase 2 - Variables.py
+++ Actividad en Clase 2 - Variables.py
@@ -1,4 +1,16 @@
+# Contar caracteres
 print(len('Ben Mino'))
+
+# Variables
 first_name = 'Ben'
 longitud = len(first_name)
+
+# Forma 1
 print('First name length:', longitud)
+
+# Forma 2
+print('First name length:', len(first_name))
+
+# Tipo de dato
+print(type(longitud))
+print(type(first_name))
