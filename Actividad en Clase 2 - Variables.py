6ba0ade087c76d4624c63278f9bbdcc10ce56868
Ben Mino <e13123@ism.edu.ec>
2026-04-14 12:35:05 -0500

    Actividad en Clase 2

--- Actividad en Clase 2 - Variables.py
+++ Actividad en Clase 2 - Variables.py
@@ -14,3 +14,9 @@ print('First name length:', len(first_name))
 # Tipo de dato
 print(type(longitud))
 print(type(first_name))
+
+# Establecer múltiples variables en una sola línea
+first_name, last_name, country, age, is_married = 'Ben', 'Mino', 'Ecuador', 15, False
+print("Primer nombre:", first_name)
+print("Apellido:", last_name)
+
