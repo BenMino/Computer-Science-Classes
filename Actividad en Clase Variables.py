1b017d70cd6887a9b54b8688df42957e3f77d740
Ben Mino <e13123@ism.edu.ec>
2026-04-15 08:23:30 -0500

    Actividad en Clases Variables

---
+++ Actividad en Clase Variables.py
@@ -0,0 +1,54 @@
+"""
+Nombre: Ben Mino
+Fecha: 2026-04-15
+Título: Actividad en Clase Variables
+"""
+
+# Ejercicios parte 1
+nombre = "Ben"
+apellido = "Mino"
+nombreCompleto = nombre + apellido
+pais = "Ecuador"
+ciudad = "Quito"
+edad = 15
+anio = 2026
+estaCasado = False
+esVerdadero = True
+luzEncendida = False
+
+color, comida, animal = "azul", "pizza", "perro"
+
+# Ejercicios parte 2
+print(type(nombre))
+print(type(apellido))
+print(type(nombreCompleto))
+print(type(pais))
+print(type(ciudad))
+print(type(edad))
+print(type(anio))
+print(type(estaCasado))
+print(type(esVerdadero))
+print(type(luzEncendida))
+
+print("Longitud de nombre:", len(nombre))
+print("¿El nombre es más largo que apellido?:", len(nombre) > len(apellido))
+
+numeroUno = 5
+numeroDos = 4
+
+total = numeroUno + numeroDos
+diferencia = numeroUno - numeroDos
+producto = numeroUno * numeroDos
+division = numeroUno / numeroDos
+residuo = numeroDos % numeroUno
+potencia = numeroUno ** numeroDos
+divisionEntera = numeroUno // numeroDos
+
+print("Suma:", total)
+print("Diferencia:", diferencia)
+print("Producto:", producto)
+print("División:", division)
+print("Residuo:", residuo)
+print("Potencia:", potencia)
+print("División entera:", divisionEntera)
+
