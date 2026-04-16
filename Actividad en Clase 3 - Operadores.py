130aec6a122b55a027aeb8dc588494b86a716c6f
Ben Mino <e13123@ism.edu.ec>
2026-04-16 13:47:02 -0500

    Actividad en Clase 3 - Operadores

---
+++ Actividad en Clase 3 - Operadores.py
@@ -0,0 +1,14 @@
+# Edad entero
+edad = 15
+print("Edad:", edad)
+
+# Estatura dcimal
+estatura = 1.75
+print("Estatura:", estatura)
+
+# Área de un triángulo
+base = float(input("Ingresa la base del triángulo: "))
+altura = float(input("Ingresa la altura del triángulo: "))
+area_triangulo = 0.5 * base * altura
+print("Área del triángulo:", area_triangulo)
+
