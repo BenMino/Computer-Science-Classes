772eb331834fbbd7b27b52d09051d97ac9a6bc6f
Ben Mino <e13123@ism.edu.ec>
2026-04-21 12:33:46 -0500

    Actividad 4 (Strings)

--- Actividad 4 (Strings).py
+++ Actividad 4 (Strings).py
@@ -20,4 +20,30 @@ print(f"Mi nombre es {name} y mi apellido es {last_name}")
 language = "Python"
 a, b, c, d, e, f = language
 print(a)
+print(c)
+print(language[0])
+print(language[0:3])
+print(language[0:5])
+print(language[-3])
+
+greeting = "Hello, World!"
+print(greeting[::-1])
+
+language = "Python"
+pto = language[0:6:2]
+print(pto)
+yhn = language[1:6:2]
+print(yhn)
+
+challenge = 'thirty days of python'
+print(challenge.capitalize())
+
+challenge = 'thirty days of python'
+print(challenge.count('y'))
+print(challenge.count('y', 7, 14))
+print(challenge.count('th'))
+
+
+
+
 
