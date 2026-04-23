d7cf1165c77fb1122419bf5d229fd2b88f324c05
Ben Mino <e13123@ism.edu.ec>
2026-04-23 14:13:04 -0500

    Notas en Clase 5

--- Notas de Clase 5 (Condicionales).py
+++ Notas de Clase 5 (Condicionales).py
@@ -1,15 +1,15 @@
 a = int(input('Enter a grade over 100: '))
-if a >= 90 and a <= 100:
-    print(f'{a} is equal to A')
+if a > 100 or a < 0:
+    print('Invalid grade')
 else:
-    if a >= 80 and a <= 89:
-        print(f'{a} is equal to B')
+    if a >= 90 and a <= 100:
+        print(f'{a} is equal to A')
     else:
-        if a >= 70 and a <= 79:
-            print(f'{a} is equal to C')
+        if a >= 80 and a <= 89:
+            print(f'{a} is equal to B')
         else:
-            if a >= 60 and a <= 69:
-                print(f'{a} is equal to D')
+            if a >= 70 and a <= 79:
+                print(f'{a} is equal to C')
             else:
                 print(f'{a} is equal to F')
 print('This is the end of the program')
