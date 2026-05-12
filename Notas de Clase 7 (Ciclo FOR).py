d677d2d45a25a7804eead31f492bf1d7a92563f4
Ben Mino <e13123@ism.edu.ec>
2026-05-12 12:34:00 -0500

    Notas de Clase 7 (Ciclo FOR)

--- Notas de Clase 7 (Ciclo FOR).py
+++ Notas de Clase 7 (Ciclo FOR).py
@@ -1,4 +1,4 @@
-language = 'Python'
+language = "P y t h o n"
 for letter in language:
     print(letter)
 
@@ -14,3 +14,16 @@ for value in values:
     sum += value
 average = sum/counter
 print(f"The average is: {average}")
+
+word = input("Enter a word: ")
+vowels = 0
+consonant = 0
+for letter in word:
+    if letter in "aeiou" or letter in "AEIOU":
+        vowels += 1
+    else:
+        consonant += 1
+    lenght = len(word)
+print(f"The number of vowels is: {vowels}")
+print(f"The number of consonants is: {consonant}")
+print(f"The total of words is: {lenght}")
