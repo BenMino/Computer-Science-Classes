c9fbb928468b259eb93d26de23706b395b086a6d
Ben Mino <e13123@ism.edu.ec>
2026-05-12 12:10:34 -0500

    Notas de Clase 7 (Ciclo FOR)

---
+++ Notas de Clase 7 (Ciclo FOR).py
@@ -0,0 +1,14 @@
+language = 'Python'
+for letter in language:
+    print(letter)
+
+numbers = [0, 1, 2, 3, 4, 5]
+for number in numbers:
+    print(number)
+
+values = [2, 4, 6, 8, 10]
+sum = 0
+for value in values:
+    sum = sum + value
+average = sum/len(values)
+print(average)
